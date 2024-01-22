27c8463c98a0a200defe771db55e92641c3da697
ratul-space <to.ratul.hosen@gmail.com>
2024-01-22 08:35:41 +0600

    feat: optimize code

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -5,18 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.mvvmrecipeapp.ui.theme.MVVMRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
