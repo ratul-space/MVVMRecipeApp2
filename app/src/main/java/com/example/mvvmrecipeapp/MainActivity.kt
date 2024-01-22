fef43a0e6a60073982444c58465cae263163fd5d
ratul-space <to.ratul.hosen@gmail.com>
2024-01-22 11:48:02 +0600

    feat: makeing of recipe app starting

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -17,11 +17,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,67 +33,48 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+
         setContent {
             Column {
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                        .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
-                    verticalArrangement = Arrangement.SpaceAround
-                ) {
-                    Text(
-                        text = "ITEM 1",
-                        Modifier.align(Alignment.CenterHorizontally)
-                    )
-                    Text(
-                        text = "ITEM 1",
-                        Modifier.align(Alignment.CenterHorizontally)
-                    )
-                }
-                Spacer(Modifier.padding(20.dp))
-                Row(
-                    Modifier
-                        .width(200.dp)
-                        .height(200.dp)
-                        .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = "ITEM 2",
+                Image(
+                    bitmap = ImageBitmap.imageResource(R.drawable.img_pexels_jonathan_borba),
+                    contentDescription = null,
+                    Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                Column {
+                    Row(
+                        Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Happy meal",
+                            color = Color.Black,
+                            fontSize = 30.sp
+                        )
+                        Text(
+                            text = "200 TK",
+                            fontSize = 24.sp,
+                            color = Color.Blue
+                        )
                         Modifier.align(Alignment.CenterVertically)
+                    }
+                    Spacer(Modifier.padding(10.dp))
+                    Text(
+                        text = "800 calories",
+                        fontSize = 27.sp,
                     )
+                    Spacer(Modifier.padding(10.dp))
+                    Button(
+                        onClick = {},
+                        Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(text = "ORDER NOW")
+                    }
                 }
             }
-
-
         }
-
-
-//        setContent {
-//            Column {
-//                Image(
-//                    bitmap = ImageBitmap.imageResource(R.drawable.img_pexels_jonathan_borba),
-//                    contentDescription = null
-//                )
-//
-//                Column {
-//                    Text(
-//                        text = "Happy meal",
-//                        color = Color.Gray,
-//                        fontSize = 34.sp
-//                    )
-//                    Text(
-//                        text = "800 calories",
-//                        fontSize = 27.sp,
-//                    )
-//                    Text(
-//                        text = "200 TK",
-//                        fontSize = 30.sp,
-//                        color = Color.Blue
-//                    )
-//                }
-//            }
-//        }
     }
 }
