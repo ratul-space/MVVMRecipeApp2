d70fdf91d1b6345ada122a2ac56fdb8988c62d0c
ratul-space <to.ratul.hosen@gmail.com>
2024-01-22 10:56:53 +0600

    feat: make a layout with text

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -1,29 +1,44 @@
 package com.example.mvvmrecipeapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Text(text = "Hey look some text")
-                Spacer(modifier = Modifier.padding(top = 10.dp))
-                Button(
-                    onClick = {}
-                ) {
-                    Text(text = "A button")
+            Column {
+                Image(
+                    bitmap = ImageBitmap.imageResource(R.drawable.img_pexels_jonathan_borba),
+                    contentDescription = null
+                )
+
+                Column {
+                    Text(
+                        text = "Happy meal",
+                        color = Color.Gray,
+                        fontSize = 34.sp
+                    )
+                    Text(
+                        text = "800 calories",
+                        fontSize = 27.sp,
+                    )
+                    Text(
+                        text = "200 TK",
+                        fontSize = 30.sp,
+                        color = Color.Blue
+                    )
                 }
             }
         }
