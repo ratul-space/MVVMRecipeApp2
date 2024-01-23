390b6e27663b5b3cfbf3c4bd3511774d9ad1ccea
ratul-space <to.ratul.hosen@gmail.com>
2024-01-23 19:18:44 +0600

    feat: make a animation

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.mvvmrecipeapp
 
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
--- app/src/main/java/com/example/mvvmrecipeapp/RecipeListFragment.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/RecipeListFragment.kt
@@ -4,19 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.contextaware.ContextAware
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 
 class RecipeListFragment : androidx.fragment.app.Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                Text(text = "Recipe list fragment")
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
+        view.findViewById<ComposeView>(R.id.compose_view).setContent {
+            Column(
+                modifier = Modifier
+                    .border(border = BorderStroke(1.dp, color = Color.Black))
+                    .padding(16.dp)
+            ) {
+                Text(text = "THIS IS A COMPOSABLE INSIDE THE FRAGMENT")
+                Spacer(modifier = Modifier.padding(10.dp))
+                CircularProgressIndicator()
+                Spacer(modifier = Modifier.padding(10.dp))
+                Text(text = "NEAT")
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                val customView = HorizontalDottedProgress(requireContext())
+                AndroidView({
+                    customView
+                })
+
             }
         }
+        return view
     }
 }
