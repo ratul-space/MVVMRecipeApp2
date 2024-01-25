3b61cb6d97b0307b2a15baeeebe0af448ebc8102
ratul-space <to.ratul.hosen@gmail.com>
2024-01-25 22:38:51 +0600

    feat: add Recipe data class

--- app/src/main/java/com/example/mvvmrecipeapp/Recipe.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/Recipe.kt
@@ -5,5 +5,16 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Recipe(
-    val id: Int?,
-) : Parcelable
+    val id: Int? = null,
+    val title: String? = null,
+    val publisher: String? = null,
+    val featuredImage: String? = null,
+    val rating: Int? = 0,
+    val sourceUrl: String? = null,
+    val description: String? = null,
+    val cookingInstructions: String? = null,
+    val ingredients: List<String> = listOf(),
+    val dateAdded: String? = null,
+    val dateUpdated: String? = null
+
+    ) : Parcelable
