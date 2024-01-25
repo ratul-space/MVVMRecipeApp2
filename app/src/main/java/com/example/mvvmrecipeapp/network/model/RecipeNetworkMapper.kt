eb95b086bc053b9c76cb61138e5fb1aa0f254952
ratul-space <to.ratul.hosen@gmail.com>
2024-01-25 22:44:30 +0600

    refactor: optimize imports on RecipeNetworkMapper

--- app/src/main/java/com/example/mvvmrecipeapp/network/model/RecipeNetworkMapper.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/network/model/RecipeNetworkMapper.kt
@@ -2,7 +2,6 @@ package com.example.mvvmrecipeapp.network.model
 
 import com.example.mvvmrecipeapp.domain.model.Recipe
 import com.example.mvvmrecipeapp.domain.util.EntityMapper
-import com.example.mvvmrecipeapp.network.model.RecipeNetworkEntity
 
 class RecipeNetworkMapper : EntityMapper<RecipeNetworkEntity, Recipe> {
     override fun mapFromEntity(entity: RecipeNetworkEntity): Recipe {
